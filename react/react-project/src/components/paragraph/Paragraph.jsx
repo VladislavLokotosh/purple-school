b8be1f39b9307a00c493b7576784843e39b3e366
Vladislav <lokotoshv@yahoo.com>
2026-02-27 14:39:46 +0300

    1-start

---
+++ react/react-project/src/components/paragraph/Paragraph.jsx
@@ -0,0 +1,9 @@
+import './Paragraph.css'
+
+function Paragraph(){
+    return(
+        <p className='main-paragraph'>Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</p>
+    )
+}
+
+export default Paragraph
